bc53ff4656977df7d9408ae5f0268d83fb004804
Casey Kelly <casey.kelly@colorado.edu>
2018-01-28 18:37:05 -0700

    update comments spec after new failure

--- test/reducers/comments_test.js
+++ test/reducers/comments_test.js
@@ -4,7 +4,7 @@ import { SAVE_COMMENT } from "../../src/actions/types";
 
 describe("Comments Reducer", () => {
   it("handles action with unknown type", () => {
-    expect(commentReducer()).to.be.instanceof(Array);
+    expect(commentReducer(undefined, {})).to.be.instanceof(Array);
   });
 
   it("handles action of type SAVE_COMMENT", () => {
