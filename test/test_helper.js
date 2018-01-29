1bfcf356ac6efd124e2380ed3aef7c4bd2f69805
Casey Kelly <casey.kelly@colorado.edu>
2018-01-28 19:45:14 -0700

    add TestUtils and ReactDOM

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,6 +1,14 @@
 import jsdom from "jsdom";
 import jquery from "jquery";
+import TestUtils from "react-addons-test-utils";
+import ReactDOM from "react-dom";
 
 global.document = jsdom.jsdom("<!doctype html><html><body></body></html>");
 global.window = global.document.defaultView;
 const $ = jquery(global.window);
+
+function renderComponent(ComponentClass) {
+  const componentInstance = TestUtils.renderIntoDocument(<ComponentClass />);
+
+  return $(ReactDOM.findDOMNode(componentInstance));
+}
